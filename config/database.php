6031a6cc995c917b3f785fad612e1abecc294586
Zeyltar <florian.poinsot@ynov.com>
2022-06-10 17:46:01 +0200

    Convert to use Heroku PostgreSQL database

--- config/database.php
+++ config/database.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Support\Str;
 
+$host = "127.0.0.1";
+$username = "forge";
+$password = "";
+$database = "forge";
+
+$url = parse_url(getenv("DATABASE_URL"));
+
+if(empty($url)) {
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+}
+
 return [
 
     /*
@@ -15,7 +29,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +80,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', $host),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', $database),
+            'username' => env('DB_USERNAME', $username),
+            'password' => env('DB_PASSWORD', $password),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
