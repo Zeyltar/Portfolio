86743b5cd19587b95fc55882d35037cde49f01a9
Zeyltar <florian.poinsot@ynov.com>
2022-06-18 00:42:54 +0200

    updated style & wip list

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,31 +4,53 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="icon" type="image/svg" href="/favicon.svg">
+    <link rel="preload" href="css/app.css" as="style">
     <link href="css/app.css" rel="stylesheet">
     <title>🔨 Portfolio 🔨</title>
 </head>
-<body class="bg-zinc-900 font-raleway">
-    <header class="header sticky flex bg-zinc-900 shadow-2xl w-full">
-        <div class="logo text-neutral-900 text-2xl tracking-widest font-bold bg-zinc-50 box-content pl-1 pr-1 m-3 w-fit group hover:shadow-[0_0_15px_rgba(0_0_0/0.1),_0_0_6px_rgba(0_0_0/0.1)] hover:shadow-cyan-300 hover:transition-all">
-            <a class="" href="/">PORTFOLIO</a>
+<body class="bg-zinc-900">
+    <header class="navbar">
+        <div class="logo">
+            <a href="/">PORTFOLIO</a>
         </div>
     </header>
-
-    <footer class=" w-full items-center bg-zinc-900 text-base text-neutral-50 py-8">
+    <section class="main py-8">
+        <div class="container mx-auto">
+            <ul class="relative flex flex-col interactive-links">
+                <li class="relative item">
+                    <a href="" class="title link">LINK 1</a>
+                    <p class="subtitle"></p>
+                </li>
+                <li class="relative item">
+                    <a href="" class="title link">LINK 2</a>
+                    <p class="subtitle"></p>
+                </li>
+                <li class="relative item">
+                    <a href="" class="title link">LINK 3</a>
+                    <p class="subtitle"></p>
+                </li>
+                <li class="relative item">
+                    <a href="" class="title link">LINK 4</a>
+                    <p class="subtitle"></p>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <footer class="footer items-center">
         <hr class="w-1/2 mx-auto border-zinc-600">
-        <div class="grid grid-flow-col pt-8">
+        <div class="grid grid-flow-col py-8">
             <p class="justify-self-end px-8" >© 2022 Florian Poinsot</p>
-            <ul class="flex list-none pl-8 px-8 border-l border-l-zinc-600 child:my-auto child:mr-6 last:child:mr-0">
-                <li class="group">
-                    <a href="https://github.com/Zeyltar" target="_blank">
-                        <svg class="fill-neutral-50 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+            <ul class="flex list-none pl-8 px-8 border-l border-l-zinc-600 child:my-auto child:mr-6 last:child:mr-0 group">
+                <li class="">
+                    <a class="" href="https://github.com/Zeyltar" target="_blank">
+                        <svg class="fill-neutral-50 w-6 hover:fill-teal-400 duration-300 transition-all" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
                             <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/>
                         </svg>
                     </a>
                 </li>
                 <li>
                     <a href="https://www.linkedin.com/in/florian-poinsot-/" target="_blank">
-                        <svg class="fill-neutral-50 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                        <svg class="fill-neutral-50 w-6 hover:fill-teal-400 duration-300 transition-all" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                             <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/>
                         </svg>
                     </a>
